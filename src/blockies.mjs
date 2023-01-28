bbb8bae3a283b215ca5b3bbb20e4f646378e8a2a
carrotn0se <carrotnose@proton.me>
2023-01-29 03:25:53 +0800

    Added options to set color, bgcolor, spotcolor with seperate seeds in options

--- src/blockies.mjs
+++ src/blockies.mjs
@@ -1,33 +1,37 @@
 // The random number is a js implementation of the Xorshift PRNG
-const randseed = new Array(4); // Xorshift: [x, y, z, w] 32 bit values
+let randseeds = {}; 
 
-function seedrand(seed) {
-	randseed.fill(0);
+function seedrand(seed, target) {
+	randseeds[target] = new Array(4) // Xorshift: [x, y, z, w] 32 bit values
+	randseeds[target].fill(0);
 
 	for(let i = 0; i < seed.length; i++) {
-		randseed[i%4] = ((randseed[i%4] << 5) - randseed[i%4]) + seed.charCodeAt(i);
+		randseeds[target][i%4] = ((randseeds[target][i%4] << 5) - randseeds[target][i%4]) + seed.charCodeAt(i);
 	}
 }
 
-function rand() {
+function rand(target) {
+	if(!randseeds[target]){
+		target="fallback"
+	}
 	// based on Java's String.hashCode(), expanded to 4 32bit values
-	const t = randseed[0] ^ (randseed[0] << 11);
+	const t = randseeds[target][0] ^ (randseeds[target][0] << 11);
 
-	randseed[0] = randseed[1];
-	randseed[1] = randseed[2];
-	randseed[2] = randseed[3];
-	randseed[3] = (randseed[3] ^ (randseed[3] >> 19) ^ t ^ (t >> 8));
+	randseeds[target][0] = randseeds[target][1];
+	randseeds[target][1] = randseeds[target][2];
+	randseeds[target][2] = randseeds[target][3];
+	randseeds[target][3] = (randseeds[target][3] ^ (randseeds[target][3] >> 19) ^ t ^ (t >> 8));
 
-	return (randseed[3] >>> 0) / ((1 << 31) >>> 0);
+	return (randseeds[target][3] >>> 0) / ((1 << 31) >>> 0);
 }
 
-function createColor() {
+function createColor(target) {	
 	//saturation is the whole color spectrum
-	const h = Math.floor(rand() * 360);
+	const h = Math.floor(rand(target) * 360);
 	//saturation goes from 40 to 100, it avoids greyish colors
-	const s = ((rand() * 60) + 40) + '%';
+	const s = ((rand(target) * 60) + 40) + '%';
 	//lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
-	const l = ((rand() + rand() + rand() + rand()) * 25) + '%';
+	const l = ((rand(target) + rand(target) + rand(target) + rand(target)) * 25) + '%';
 
 	return 'hsl(' + h + ',' + s + ',' + l + ')';
 }
@@ -45,7 +49,7 @@ function createImageData(size) {
 		for(let x = 0; x < dataWidth; x++) {
 			// this makes foreground and background color to have a 43% (1/2.3) probability
 			// spot color has 13% chance
-			row[x] = Math.floor(rand()*2.3);
+			row[x] = Math.floor(rand("pattern")*2.3);
 		}
 		const r = row.slice(0, mirrorWidth);
 		r.reverse();
@@ -64,18 +68,35 @@ function buildOpts(opts) {
 
 	newOpts.seed = opts.seed || Math.floor((Math.random()*Math.pow(10,16))).toString(16);
 
-	seedrand(newOpts.seed);
+	seedrand(newOpts.seed, "fallback"); // used as fallback when certain seeds aren't defined
+	if (opts.patternseed){
+		newOpts.patternseed = opts.patternseed;
+		seedrand(newOpts.patternseed, "pattern");
+	}
+	if (opts.colorseed){
+		newOpts.colorseed = opts.colorseed;
+		seedrand(newOpts.colorseed, "color");
+	}
+	if (opts.bgcolorseed){
+		newOpts.bgcolorseed = opts.bgcolorseed;
+		seedrand(newOpts.bgcolorseed, "bgcolor");
+	}
+	if (opts.spotcolorseed){
+		newOpts.spotcolorseed = opts.spotcolorseed;
+		seedrand(newOpts.spotcolorseed, "spotcolor");
+	}
 
 	newOpts.size = opts.size || 8;
 	newOpts.scale = opts.scale || 4;
-	newOpts.color = opts.color || createColor();
-	newOpts.bgcolor = opts.bgcolor || createColor();
-	newOpts.spotcolor = opts.spotcolor || createColor();
+	newOpts.color = opts.color || createColor("color");
+	newOpts.bgcolor = opts.bgcolor || createColor("bgcolor");
+	newOpts.spotcolor = opts.spotcolor || createColor("spotcolor");
 
 	return newOpts;
 }
 
 export function renderIcon(opts, canvas) {
+	randseeds = {}
 	opts = buildOpts(opts || {});
 	const imageData = createImageData(opts.size);
 	const width = Math.sqrt(imageData.length);
